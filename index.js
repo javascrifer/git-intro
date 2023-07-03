0bf95546c50fc7853d85438724fa44b5affba9db
Islam Ismayil <islami@wix.com>
2023-07-03 15:59:31 +0300

    whatever

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function iWillDefinitelyHaveConflicts(a, b) {
   const iWillCreateConflicts = () => {
     const _a = a + 1;
     const _b = b + 2;
-    return _a + _b;
+    return _a + _b + 10;
   }
   
   return iWillCreateConflicts();
