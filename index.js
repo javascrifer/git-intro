8485aaa7fc7cfd7ca7569a746b88a2a2e48d7bf0
I.I <islami@wix.com>
2023-07-03 15:43:56 +0300

    rebase (#3)

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function iWillHaveConflicts(a, b) {
-  const _a = a + 1;
-  const _b = b + 2;
-  return _a + _b;
+function iWillDefinitelyHaveConflicts(a, b) {
+  const iWillCreateConflicts = () => {
+    const _a = a + 1;
+    const _b = b + 2;
+    return _a + _b;
+  }
+  
+  return iWillCreateConflicts();
 }
 
-console.log(iWillHaveConflicts(10, 12))
+console.log(iWillDefinitelyHaveConflicts(10, 12));
